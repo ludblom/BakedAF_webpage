5de551c8726c1e4b03df2eb2b629c4225f182763
Ludvig Blomkvist <ludblom@fripost.org>
2020-12-05 06:29:34 +0100

    Working split function

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from split_wiser import start
 
 if __name__ == "__main__":
     people = [
-        ['Lucas', 0],
-        ['Erik', 0],
-        ['Miriam', 91],
-        ['Emma', 100],
-        ['Ludde', 103],
-        ['Musen', 400],
-        ['Daniel', 600]
+        ('Lucas', 23),
+        ('Erik', 0),
+        ('Miriam', 91),
+        ('Emma', 100),
+        ('Ludde', 103),
+        ('Musen', 400),
+        ('Daniel', 600)
     ]
 
     print(start(people))
