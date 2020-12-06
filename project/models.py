b31999a6ff09fd3e1f47e9996e7939d460b82f7c
Ludvig Blomkvist <ludblom@student.chalmers.se>
2020-12-06 15:37:06 +0100

    Working on the database

--- project/models.py
+++ project/models.py
@@ -6,3 +6,9 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     password = db.Column(db.String(100))
     uname = db.Column(db.String(100))
+    splitdata = db.relationship('SplitData', backref='author', lazy='dynamic')
+
+
+class SplitData(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    uname = db.Column(db.String(100), db.ForeignKey('user.uname'))
